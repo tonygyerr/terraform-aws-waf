86be4af02ba815e67b56c9cdec54cb3fdc87c4ee
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-12-31 03:22:53 -0500

    wafv2

--- waf_v2.tf
+++ waf_v2.tf
@@ -557,3 +557,9 @@ resource "aws_wafv2_web_acl_logging_configuration" "main" {
     }
   }
 }
+
+resource "aws_wafv2_web_acl_association" "main" {
+  count        = var.associate_alb ? 1 : 0
+  resource_arn = var.alb_arn
+  web_acl_arn  = aws_wafv2_web_acl.main.arn
+}
